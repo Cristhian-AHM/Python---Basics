4536edcce0b3af9ec9e41a3ff79151594684c557
Cristhian-AHM <14111367@itcj.edu.mx>
2018-09-25 12:39:53 -0600

    Tipos de Variables

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,6 +1,25 @@
-for n in "Hola":
-    print(n)
-    pass
-print("Done")
+#Ejemplos de Variables
+
+a = 42; #Valor Entero
+ahex = 0x2a; #Valor en notación hexadecimal
+
+b = 3.1415; #Valor como flotante
+bnotacion = 3.14e0; #Valor en notación base
+
+c = 3 + 7j; #Python soporta números complejos
+d = "Ejemplo de cadena de caracteres" #Cadenas
+
+#Imprimir variables en pantalla
+
+print (a,b,c,d);
+
+#Tipos de las variables
+print(type(a));
+print(type(ahex));
+print(type(b));
+print(type(bnotacion));
+print(type(c));
+print(type(d));
+
 
 
